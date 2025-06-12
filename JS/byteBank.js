b2307cdc1f36d2ffc3a69358d21c4752b36836e7
Nawldo <nalddu@yahoo.com.b>
2025-06-11 21:41:06 -0300

    aprendendo typescript

---
+++ JS/byteBank.js
@@ -0,0 +1,27 @@
+// Define o saldo inicial da conta bancária
+let saldo = 3000; 
+
+// Captura o elemento HTML onde o saldo será exibido
+const elementoSaldo = document.querySelector(".saldo-valor .valor");
+
+// Atualiza o conteúdo do elemento com o saldo atual
+elementoSaldo.textContent = saldo;
+
+// Captura o formulário de nova transação na interface
+const elementoFormulario = document.querySelector(".block-nova-transacao form");
+
+// Adiciona um ouvinte de evento para a submissão do formulário
+elementoFormulario.addEventListener("submit", function(event) {
+
+    // Previne o comportamento padrão do formulário (recarregar a página)
+    event.preventDefault();
+
+    // Verifica se todos os campos obrigatórios foram preenchidos
+    if (!elementoFormulario.checkValidity()){
+        // Exibe um alerta caso algum campo esteja inválido
+        alert("Por favor, preencha todos os campos da transação!");
+        
+        // Interrompe a execução do código para evitar o processamento de uma transação inválida
+        return;
+    }
+});
